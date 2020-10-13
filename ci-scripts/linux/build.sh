879ea7a11c6894e347a78df186ba15cadab06b72
Alex Spataru <alex_spataru@outlook.com>
2020-10-12 19:20:16 -0500

    Update build.sh

--- ci-scripts/linux/build.sh
+++ ci-scripts/linux/build.sh
@@ -1,8 +1,3 @@
-wget -q -O qt5.zip https://github.com/francescmm/ci-utils/releases/download/qt/qt5.zip;
-unzip -qq qt5.zip;
-export QTDIR=$PWD/qt5
-export PATH=$QTDIR/bin:$PATH;
-export QT_PLUGIN_PATH=$PWD/qt5/plugins;
 mkdir build
 cd build
 g++ --version
