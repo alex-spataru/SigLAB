300c9e2d6842ff59d021cde0e160fc74cd7efddb
Alex Spataru <alex_spataru@outlook.com>
2020-10-12 19:19:54 -0500

    Update install.sh

--- ci-scripts/linux/install.sh
+++ ci-scripts/linux/install.sh
@@ -5,6 +5,7 @@ sudo add-apt-repository -y ppa:jonathonf/gcc-9
 sudo apt-get update
 sudo apt-get -qq install libc6-i386 build-essential libgl1-mesa-dev mesa-common-dev libgles2-mesa-dev libxkbcommon-x11-0 libxcb-icccm4-dev libxcb-xinerama0 libxcb-image0 libxcb-keysyms1
 sudo apt-get -qq install libxcb-*
+sudo apt-get -qq qtbase5-dev qt5-default libqt5opengl5 qml-module-qtquick-controls2 libqt5charts5 libqt5charts5-dev
 sudo apt-get -qq remove gcc g++
 sudo apt-get install gcc-9 g++-9
 sudo ln -s /usr/bin/g++-9 /usr/bin/g++
